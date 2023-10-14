e5bd10778fa48713d90681898bc092502dc11591
SateeshKurapati <142497513+SateeshKurapati@users.noreply.github.com>
2023-10-14 16:54:07 +0530

    Add files via upload

---
+++ sql pratice orderby and distinct.sql
@@ -0,0 +1,11 @@
+use company
+select * from books order by bid desc
+select * from books order by bid asc
+select * from books order by bname desc
+select * from books order by bname asc
+select * from books order by price desc
+select * from books order by price asc
+select  distinct price from books
+select distinct bid from books
+select distinct bname from books
+select distinct bid,bname from books
