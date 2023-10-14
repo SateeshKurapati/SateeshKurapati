d25fd077a14e0c7a717405fe1999c94089cef396
SateeshKurapati <142497513+SateeshKurapati@users.noreply.github.com>
2023-10-14 16:51:02 +0530

    Add files via upload

---
+++ sql pratice column.sql
@@ -0,0 +1,4 @@
+use college
+select * from student
+select max(marks), min(marks), sum(marks), avg(marks), count(marks) from student
+select max(marks) as maximum, min(marks) as minimum, sum(marks), avg(marks), count(marks) from student
