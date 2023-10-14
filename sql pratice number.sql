e81ad547ae06c48a791e4c49c4d6cdc6eed36408
SateeshKurapati <142497513+SateeshKurapati@users.noreply.github.com>
2023-10-14 16:49:06 +0530

    Add files via upload

---
+++ sql pratice number.sql
@@ -0,0 +1,17 @@
+select abs(-45.6)
+select abs(price)from books
+select sign(45.6)
+select sign(0)
+select price,sign(bid)from books
+select sqrt(price)from books
+select ceil(45.3)
+select floor(45.9)
+select round(45.67)
+select round(45.63)
+select round(45.639,2)
+select power(2,3)
+select exp(2)
+select log(10,2)
+select pi()
+select tan(45*pi()/180)
+select sin(30*pi()/180)
