7fa9217b620ea53b7110d3533f38e81e09bbce4e
SateeshKurapati <142497513+SateeshKurapati@users.noreply.github.com>
2023-10-15 22:30:51 +0530

    Add files via upload

---
+++ sql practice auto_increment.sql
@@ -0,0 +1,5 @@
+use practice
+create table student(sid int primary key auto_increment, sname varchar(20))
+insert into student(sname) values('parsha'),('akhil'),('amar')
+select * from student 
+insert into student(sname) values('haleem')
